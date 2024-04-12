498a38e236abe726284354ec6a8b4cf88879ecb8
Shohin Abdulkhamidov <abd.shohin@gmail.com>
2024-04-12 15:42:53 -0700

    Scaffold App

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+import CheckoutButton from "./CheckoutButton";
+import EmbeddedCheckoutButton from "./EmbeddedCheckoutButton";
+
+export default function Home() {
+  return (
+    <>
+      <CheckoutButton />
+      <EmbeddedCheckoutButton />
+    </>
+  );
+}
